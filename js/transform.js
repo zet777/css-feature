0e8c3f68d7220a46052b189586f2ba18dab6a9a9
rzet777 <rzet777@gmail.com>
2018-07-16 17:11:33 +0300

    Модифицировал меню.

--- js/transform.js
+++ js/transform.js
@@ -4,4 +4,5 @@ $('.card').on('click', function () {
 $('.menu-btn').on('click', function (e) {
 	e.preventDefault; /*отменяем стандартное событие*/
 	$(this).toggleClass('menu-btn_active');
+	$('.menu-nav').toggleClass('menu-nav_active');
 })
